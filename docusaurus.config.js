2791cf151d017f3178aae4d80d776f1435b0a6b3
Hexane911 <koryaminmih@gmail.com>
2024-11-29 11:08:54 +0300

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,23 +15,14 @@ const config = {
     "Mighty Widget supercharges online learning with powerful gamification tools.",
   favicon: "img/logo.png",
 
-  // Set the production url of your site here
   url: "https://hexane911.github.io/mighty-docusaurus/",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/mighty-docusaurus/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: "Hexane911", // Usually your GitHub org/user name.
   projectName: "mighty-docusaurus", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -43,10 +34,6 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -59,7 +46,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       navbar: {
         title: "Mighty Widget",
         logo: {
@@ -84,19 +70,6 @@ const config = {
               {
                 label: "Tutorial",
                 to: "/docs/installation",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "X",
-                href: "https://x.com/docusaurus",
               },
             ],
           },
